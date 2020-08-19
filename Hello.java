54f56e246ccf4b7ded188d2f2810644f002589b6
BrendanJamieson <69919608+BrendanJamieson@users.noreply.github.com>
2020-08-19 14:09:17 -0400

    Notepad copy of Lab 1

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Brendan Jamieson");
+		System.out.println("Hello Java Lab One");
+		System.out.println("First Java Lab");
+		System.out.println("08/19/2002");
+		System.out.println("CMSC255 Section 02");
+		System.out.println();
+	}
+}
